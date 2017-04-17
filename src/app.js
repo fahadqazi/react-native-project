e6bcd547552f0196dc8c61b2b234dac3ebb2637a
Fahad Qazi <fahadqazi@gmail.com>
2017-04-17 22:25:55 +0100

    starting auth project

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+class App extends Component {
+    render() {
+        return(
+            <View>
+                <Text>Coming from the App!</Text>
+            </View>
+        );
+    }
+}
+
+export default App;
