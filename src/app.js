13303b0cb43e960b7e89fe67912b5fb02384a920
Fahad Qazi <fahadqazi@gmail.com>
2017-04-21 13:26:07 +0100

    small refactor

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { View } from 'react-native';
 import firebase from 'firebase';
 import { Header, Button, CardSection } from './components/common';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
-    constructor(){
-        super()
+    constructor() {
+        super();
 
         this.state = ({ loggedIn: null });
         this.renderContent = this.renderContent.bind(this);
@@ -14,38 +14,38 @@ class App extends Component {
 
     componentWillMount() {
         firebase.initializeApp({
-            apiKey: "AIzaSyA6hYlY4nbeb9GuA-vthnEey96d4jHTE_o",
-            authDomain: "authentication-rn-ac18b.firebaseapp.com",
-            databaseURL: "https://authentication-rn-ac18b.firebaseio.com",
-            projectId: "authentication-rn-ac18b",
-            storageBucket: "authentication-rn-ac18b.appspot.com",
-            messagingSenderId: "805454301886"
+            apiKey: 'AIzaSyA6hYlY4nbeb9GuA-vthnEey96d4jHTE_o',
+            authDomain: 'authentication-rn-ac18b.firebaseapp.com',
+            databaseURL: 'https://authentication-rn-ac18b.firebaseio.com',
+            projectId: 'authentication-rn-ac18b',
+            storageBucket: 'authentication-rn-ac18b.appspot.com',
+            messagingSenderId: '805454301886'
         });
 
         firebase.auth().onAuthStateChanged((user) => {
-            if (user){
-                this.setState({ loggedIn: true })
+            if (user) {
+                this.setState({ loggedIn: true });
             } else {
-                this.setState({ loggedIn: false })
+                this.setState({ loggedIn: false });
             }
-        })
+        });
     }
 
     renderContent() {
-        if (this.state.loggedIn){
-            return(
+        if (this.state.loggedIn) {
+            return (
                 <CardSection>
                     <Button>Log Out</Button>
                 </CardSection>
             );
         }
-        return <LoginForm />
+        return <LoginForm />;
     }
     
     render() {
-        return(
+        return (
             <View>
-                <Header headerText="Authentication"/>
+                <Header headerText="Authentication" />
                 {this.renderContent()}
             </View>
         );
